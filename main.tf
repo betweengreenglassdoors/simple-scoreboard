e1d3a91d1c8ca03860c45d5e086d8dfb6eecc752
Fairman <fairmangd@l02308721.niaid.nih.gov>
2025-01-15 14:31:21 -0500

    feat: log app activity in a CloudWatch log group

--- main.tf
+++ main.tf
@@ -16,3 +16,21 @@ provider "aws" {
 }
 
 data "aws_partition" "current" {}
+
+# Log app (Lambda function) activity in a CloudWatch log group.
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/lambda/${var.STACK_NAME}"
+  retention_in_days = 3
+}
+
+# Store players' scores in a DynamoDB table.
+resource "aws_dynamodb_table" "this" {
+  name         = "simple-scoreboard"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "name"
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+}
